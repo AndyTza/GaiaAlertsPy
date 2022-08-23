4807354ee38c836c9e5bdcbb9eeb7b2793627aec
Andy Tzanidakis <atzanida@uw.edu>
2022-08-23 08:52:36 -0700

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,64 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
+# -- Path setup --------------------------------------------------------------
 
-project = 'GaiaAlertsPy'
-copyright = '2022, Tzanidakis'
-author = 'Anastasios Tzanidakis'
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
-release = '0.1'
-version = '0.1.0'
 
-# -- General configuration
+# -- Project information -----------------------------------------------------
 
+project = "GaiaAlertPy"
+copyright = "2022, GaiaAlertsPy"
+author = "Anastasios Tzanidakis"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_copybutton",
+    "nbsphinx",
+    "sphinx_automodapi.automodapi",
 ]
+numpydoc_show_class_members = False
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
 
-templates_path = ['_templates']
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# -- Options for HTML output
+# hide input prompts in notebooks
+nbsphinx_prompt_width = "0"
 
-html_theme = 'furo' #?!
+# -- Options for HTML output -------------------------------------------------
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "furo"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+
+# hide coppy button on outputs
+copybutton_selector = "div:not(.output_area) > div.highlight pre"
