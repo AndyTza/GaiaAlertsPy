b9369e757b1d2aed6e7dbfc025ca0b2d3a01c3d9
Andy Tzanidakis <atzanida@uw.edu>
2023-06-27 09:10:14 -0700

    addiing

--- setup.py
+++ setup.py
@@ -5,20 +5,33 @@ import os
 
 from setuptools import setup
 
-
 VERSION_TEMPLATE = """
-# Note that we need to fall back to the hard-coded version if either
-# setuptools_scm can't be imported or setuptools_scm can't determine the
-# version, so we catch the generic 'Exception'.
 try:
     from setuptools_scm import get_version
     __version__ = get_version(root='..', relative_to=__file__)
 except Exception:
-    __version__ = '{version}'
+    __version__ = '1.0.0'
 """.lstrip()
 
 setup(
     use_scm_version={'write_to': os.path.join('GaiaAlertsPy', 'history.py'),
                      'write_to_template': VERSION_TEMPLATE},
-
-)
+                         name='GaiaAlertsPy',
+                        version='1.0.0',
+                        author='Andy Tzanidakis',
+                        author_email='atzanida@uw.edu',
+                        description='Fetch Gaia Photometric Science Alerts',
+                        long_description='Fetch Gaia Photometric Science Alerts',
+                        url='https://github.com/AndyTza/GaiaAlertsPy',
+                        license='BSD-3-Clause',
+                        classifiers=[
+                            'Development Status :: 5 - Production/Stable',
+                            'Intended Audience :: Developers',
+                            'License :: OSI Approved :: BSD License',
+                            'Programming Language :: Python',
+                            'Programming Language :: Python :: 3',
+                            'Programming Language :: Python :: 3.6',
+                            'Programming Language :: Python :: 3.7',
+                            'Programming Language :: Python :: 3.8',
+                        ],
+                        packages=['GaiaAlertsPy'])
